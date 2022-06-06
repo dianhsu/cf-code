d0dc59695a8a7591b7e1de515d2a6122084f5af5
dianhsu <50162344+dianhsu@users.noreply.github.com>
2022-06-06 12:20:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,35 +3,30 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://codeforces.com/gym/*/status
 // @grant       MIT
-// @version     1.0
+// @version     1.1
 // @author      dianhsu
 // @run-at      document-idle
 // @downloadURL https://raw.githubusercontent.com/dianhsu/cf-code/main/script.js
 // @description 2022/6/4 15:41:09
 // ==/UserScript==
 
-function init(){
-  let items = document.querySelector('.status-frame-datatable').querySelectorAll('tr')
-  let reg = /\d+/g;
-  let contestId = window.location.pathname.match(reg);
-  items.forEach(function(item){
-    if(item.className === 'first-row'){
-      let th = document.createElement('th');
-      th.class = 'top right';
-      th.append("View");
-      item.append(th);
-    }else{
-      let submissionId = item.dataset.submissionId;
-      let td = document.createElement('td');
-      let a = document.createElement('a');
-      a.append('code');
-      a.title = 'code';
-      a.target = '_blank';
-      a.href = `https://cf.dianhsu.com/gym/${contestId}/submission/${submissionId}`;
-      td.append(a);
-      item.append(td);
+$(function () {
+    'use strict';
+    let $tbody = $('table.status-frame-datatable>tbody');
+    let tr = $tbody.find('tr');
+    let reg = /\d+/g;
+    let contestId = window.location.pathname.match(reg);
+    for (let i = 1; i < tr.length; ++i) {
+        let td = $(tr[i]).find('td');
+        let submissionId = tr[i].dataset.submissionId;
+        let cell = $(td[0]).find('span.hiddenSource');
+        if (cell.length > 0) {
+            let item = document.createElement("a");
+            item.href = `https://cf.dianhsu.com/gym/${contestId}/submission/${submissionId}`;
+            item.text = `${submissionId}`;
+            item.target = '_blank';
+            cell.after(item);
+            cell.remove();
+        }
     }
-  });
-};
-
-init();
+});
